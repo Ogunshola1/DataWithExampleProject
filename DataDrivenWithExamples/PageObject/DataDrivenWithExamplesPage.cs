1488108927e2b1d57552faf1b272d647680208bf
Ogunshola1 <68999986+Ogunshola1@users.noreply.github.com>
2020-07-30 19:57:31 +0100

    This is my first commit

---
+++ DataDrivenWithExamples/PageObject/DataDrivenWithExamplesPage.cs
@@ -0,0 +1,67 @@
+﻿using DataDrivenWithExamples.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDrivenWithExamples.PageObject
+{
+    class DataDrivenWithExamplesPage
+    {
+        public DataDrivenWithExamplesPage()
+        {
+            driver = Hooks1.driver;
+        }
+
+        IWebDriver driver;
+
+        IWebElement usernameInput => driver.FindElement(By.XPath("//Input[@placeholder = 'Username']"));
+
+        IWebElement emailInput => driver.FindElement(By.XPath("//Input[@placeholder = 'Email']"));
+
+        IWebElement passwordInput => driver.FindElement(By.XPath("//Input[@placeholder = 'Password']"));
+
+        IWebElement signUpButton => driver.FindElement(By.XPath("//button[@type = 'submit']"));
+
+        IWebElement userLogin => driver.FindElement(By.XPath("//a[@class='nav-link ng-binding']"));
+
+
+
+
+
+        public void EnterUsername(string Username)
+        {
+            Random randomGenerator = new Random();
+            int randomint = randomGenerator.Next(1000);
+            usernameInput.SendKeys(Username + randomint);
+            //usernameInput.SendKeys("Username");
+        }
+        public void EnterEmail(string Email)
+        {
+            Random randomGenerator = new Random();
+            int randomint = randomGenerator.Next(1000);
+            emailInput.SendKeys(Email + randomint + "@talktalk.net");
+            //emailInput.SendKeys("Email");
+        }
+
+        public void EnterPassword(string Password)
+        {
+            passwordInput.SendKeys("Password");
+        }
+
+        public void ClickSignUpButton()
+        {
+            signUpButton.Click();
+        }
+
+        public bool IsUserLoginDisplayed()
+        {
+            return userLogin.Displayed;
+        }
+        public void NavigateToWebsite(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+}
---
+++ DataDrivenWithExamples/StepDefinition/DataDrivenWithExamplesSteps.cs
@@ -0,0 +1,42 @@
+﻿using DataDrivenWithExamples.PageObject;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DataDrivenWithExamples.StepDefinition
+{
+    [Binding]
+    public class DataDrivenWithExamplesSteps
+    {
+        DataDrivenWithExamplesPage datadrivenwithexamplesPage;
+
+        public DataDrivenWithExamplesSteps()
+        {
+            datadrivenwithexamplesPage = new DataDrivenWithExamplesPage();
+        }
+
+
+        [Given(@"I navigate to ""(.*)""")]
+        public void GivenINavigateTo(string url)
+        {
+            datadrivenwithexamplesPage.NavigateToWebsite(url);
+        }
+        
+        [When(@"the User enter username text ""(.*)""")]
+        public void WhenTheUserEnterUsernameText(string Username)
+        {
+            datadrivenwithexamplesPage.EnterUsername(Username);
+        }
+        
+        [When(@"the user enter email text ""(.*)""")]
+        public void WhenTheUserEnterEmailText(string Email)
+        {
+            datadrivenwithexamplesPage.EnterEmail(Email);
+        }
+        
+        [When(@"the user enter password text ""(.*)""")]
+        public void WhenTheUserEnterPasswordText(string Password)
+        {
+            datadrivenwithexamplesPage.EnterPassword(Password);
+        }
+    }
+}
